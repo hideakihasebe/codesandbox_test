eff68f79e8c0c0ad546dfa45b5386aac2f2cb394
hideakihasebe <34244780+hideakihasebe@users.noreply.github.com>
2022-08-04 18:48:39 +0900

    todo js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+const onClickAdd = () => {
+  // テキストボックスの値を取得し。初期化する
+  const inputText = document.getElementById("input-text").value;
+  document.getElementById("input-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-low";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了リストの追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
